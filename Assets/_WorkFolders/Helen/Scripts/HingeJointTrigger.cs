478651818d5ba15a098ca6389fc7fe6d68502e22
HLam10 <helen.hoilun.lam@gmail.com>
2023-02-08 18:28:50 -0500

    Added working navigation system and cabinet. Needs to be made pretty.

--- Assets/_WorkFolders/Helen/Scripts/HingeJointTrigger.cs
+++ Assets/_WorkFolders/Helen/Scripts/HingeJointTrigger.cs
@@ -3,20 +3,18 @@ using UnityEngine.Events;
 
 public class HingeJointTrigger : MonoBehaviour
 {
-    //angle threshold to trigger if we reached limit
+   
     public float angleBetweenThreshold = 1f;
-    //State of the hinge joint : either reached min or max or none if in between
     public HingeJointState hingeJointState = HingeJointState.None;
 
-    //Event called on min reached
+    
     public UnityEvent OnMinLimitReached;
-    //Event called on max reached
     public UnityEvent OnMaxLimitReached;
 
     public enum HingeJointState { Min, Max, None }
     private HingeJoint hinge;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         hinge = GetComponent<HingeJoint>();
@@ -27,7 +25,6 @@ public class HingeJointTrigger : MonoBehaviour
         float angleWithMinLimit = Mathf.Abs(hinge.angle - hinge.limits.min);
         float angleWithMaxLimit = Mathf.Abs(hinge.angle - hinge.limits.max);
 
-        //Reached Min
         if (angleWithMinLimit < angleBetweenThreshold)
         {
             if (hingeJointState != HingeJointState.Min)
@@ -35,7 +32,7 @@ public class HingeJointTrigger : MonoBehaviour
 
             hingeJointState = HingeJointState.Min;
         }
-        //Reached Max
+
         else if (angleWithMaxLimit < angleBetweenThreshold)
         {
             if (hingeJointState != HingeJointState.Max)
@@ -43,7 +40,7 @@ public class HingeJointTrigger : MonoBehaviour
 
             hingeJointState = HingeJointState.Max;
         }
-        //No Limit reached
+   
         else
         {
             hingeJointState = HingeJointState.None;
