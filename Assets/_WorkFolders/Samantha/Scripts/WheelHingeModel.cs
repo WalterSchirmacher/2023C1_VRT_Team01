0c4e3136288605fb2f4f3156fc8139d670e440e1
Samantha Meyer <thissammeyer@gmail.com>
2023-03-03 18:27:09 -0500

    removed realtime transform from all except top handle

--- Assets/_WorkFolders/Samantha/Scripts/WheelHingeModel.cs
+++ Assets/_WorkFolders/Samantha/Scripts/WheelHingeModel.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelHingeModel : MonoBehaviour
+public class WheelHingeModel
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
