81802add5d1165f1636146ae8d550523498b3d5b
Samantha Meyer <thissammeyer@gmail.com>
2023-03-05 15:48:57 -0500

    island collision prevention

--- Assets/_WorkFolders/Samantha/Scripts/SpeedLeverController.cs
+++ Assets/_WorkFolders/Samantha/Scripts/SpeedLeverController.cs
@@ -8,6 +8,8 @@ public class SpeedLeverController : MonoBehaviour
     HingeJoint hinge;
 
     public GameObject oceanScenery;
+    public GameObject frontCD;
+    public GameObject rearCD;
 
     public float angleFraction;
     public float shipSpeed;
@@ -22,10 +24,28 @@ public class SpeedLeverController : MonoBehaviour
         // find fraction of allowed rotation that the player has used
         angleFraction = hinge.angle / (hinge.limits.max - hinge.limits.min);
 
-        // move ocean scenery towards the ship an amount based on angle fraction and ship speed
-        if (hinge.angle > 10 || hinge.angle < -10)
+        // when lever is pulled, check for collisions and move islands in an allowed direction
+        if (hinge.angle > 5)
         {
-            oceanScenery.transform.Translate(0, 0, angleFraction * shipSpeed * Time.deltaTime, Space.World);
+            if (!frontCD.GetComponent<IslandCollisionHandler>().collided)
+            {
+                oceanScenery.transform.Translate(0, 0, angleFraction * shipSpeed * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                Debug.Log("YOU RAN AGROUND!");
+            }
+        }
+        if (hinge.angle < -5)
+        {
+            if (!rearCD.GetComponent<IslandCollisionHandler>().collided)
+            {
+                oceanScenery.transform.Translate(0, 0, angleFraction * shipSpeed * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                Debug.Log("YOU RAN AGROUND!");
+            }
         }
     }
 }
